d13b76909ebe1af731444a040ff908d768e2b059
limonatasuyu <emredilek6@gmail.com>
2024-07-25 14:08:53 +0300

    router changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./HomePage";
 import CategoryPage from "./CategoryPage";
 import Layout from "./Layout";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/category/:categoryName",
+        element: <CategoryPage />,
+      },
+    ],
+  },
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <Router basename="/react-some-todo-app">
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="category/:categoryName" element={<CategoryPage />} />
-      </Route>
-    </Routes>
-  </Router>
-);
+root.render(<RouterProvider router={router} />);
 
